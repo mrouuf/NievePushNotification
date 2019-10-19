72c90b8f0d2c0149ae1ee45ededebea65505e579
mrouuf <syihaabrouuf@gmail.com>
2019-10-19 21:22:00 +0700

    initial commit

---
+++ app/src/main/java/com/example/nievepushnotification/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.nievepushnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
